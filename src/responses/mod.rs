bd626b3fcfab76a25588df07f857dc250c3d035b
Wyatt J Herkamp <wherkamp@kingtux.me>
2021-05-22 13:44:41 -0400

    Listing

--- src/responses/mod.rs
+++ src/responses/mod.rs
@@ -9,3 +9,14 @@ pub struct GenericResponse<T> {
     pub kind: String,
     pub data: T,
 }
+
+#[derive(Deserialize, Debug)]
+pub struct Listing<T> {
+    pub modhash: Option<String>,
+    pub dist: Option<i32>,
+    pub after: Option<String>,
+    pub before: Option<String>,
+    pub children: Vec<T>,
+}
+
+pub type GenericListing<T> = GenericResponse<Listing<GenericResponse<T>>>;
