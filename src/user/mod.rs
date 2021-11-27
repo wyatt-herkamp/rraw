bf7c3ba6701087f637b49a2e1541b13d44e1afc9
Wyatt J Herkamp <wherkamp@kingtux.me>
2021-11-27 00:34:13 -0500

    Trying to solve the stack overflow

--- src/user/mod.rs
+++ src/user/mod.rs
@@ -1,11 +1,9 @@
-use log::trace;
 use crate::me::Me;
 use crate::responses::comments::Comments;
 use crate::responses::submission::Submissions;
 use crate::responses::user::UserResponse;
 use crate::responses::{GenericListing, RedditListing, RedditType};
 use crate::utils::error::APIError;
-use crate::utils::error::APIError::NotFound;
 use crate::utils::options::FeedOption;
 
 /// The User Object for Reddit
@@ -23,15 +21,8 @@ impl<'a> PartialEq for User<'a> {
 impl<'a> User<'a> {
     /// Gets the about data for the user
     pub async fn about(&self) -> Result<UserResponse, APIError> {
-        let url = format!("/user/{}/about.json", &self.name);
-        let response = self.me.get(&url, false).await?;
-        if !response.status().is_success() {
-            return Err(NotFound);
-        }
-        let value = response.text().await?;
-        trace!("{}",&value);
-        let x: UserResponse = serde_json::from_str(value.as_str())?;
-        return Ok(x);
+        let string = format!("/user/{}/about.json", &self.name);
+        return self.me.get_json::<UserResponse>(&*string, false).await;
     }
     /// Comments
     pub async fn comments(&self, feed: Option<FeedOption>) -> Result<Comments, APIError> {
