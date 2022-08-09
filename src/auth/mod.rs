2bda75e04b269e3cc7812b7c3fd6ff784b570231
Behzad Alirezaei <behzad.a_ir@hotmail.com>
2022-08-09 15:41:30 +0430

    Add Refresh Token Authorization
    Signed-off-by: Behzad Alirezaei <behzad.a_ir@hotmail.com>

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -391,8 +391,9 @@ impl Authenticator for CodeAuthenticator {
             HeaderValue::from_str(&*format!(
                 "Basic {}",
                 base64::encode(format!(
-                    "{}:",
-                    self.client_id.to_owned()
+                    "{}:{}",
+                    self.client_id.to_owned(),
+                    self.client_secret.to_owned()
                 ))
             ))
             .unwrap(),
@@ -457,3 +458,159 @@ impl Authenticator for CodeAuthenticator {
 
 #[async_trait]
 impl Authorized for CodeAuthenticator {}
+
+#[derive(Clone)]
+pub struct TokenAuthenticator {
+    /// Token
+    pub token: Option<String>,
+    /// When does it expire
+    pub expiration_time: Option<u128>,
+    /// Refresh token
+    pub refresh_token: String,
+    /// Client ID
+    client_id: String,
+    /// Client Secret
+    client_secret :String,
+}
+
+impl Debug for TokenAuthenticator {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "[CodeAuthenticator] Token Defined: {} Expires At {}",
+            self.token.is_some(),
+            self.expiration_time.unwrap_or(0)
+        )
+    }
+}
+
+impl TokenAuthenticator {
+    /// Creates a new Authenticator by Refresh Token Authorization
+    /// 
+    /// Note: The "client_secret" for non-confidential clients (Installed APPs) is an empty string.
+    #[allow(clippy::new_ret_no_self)]
+    pub fn new<S: Into<String>>(
+        client_id: S,
+        client_secret: S,
+        refresh_token: S,
+    ) -> TokenAuthenticator {
+        TokenAuthenticator {
+            token: None,
+            expiration_time: None,
+            refresh_token: refresh_token.into(),
+            client_id: client_id.into(),
+            client_secret: client_secret.into(),
+        }
+    }
+}
+
+#[async_trait]
+impl Authenticator for TokenAuthenticator {
+    /// Logs in
+    async fn login(&mut self, client: &Client, user_agent: &str) -> Result<bool, Error> {
+        let url = "https://www.reddit.com/api/v1/access_token";
+        let body = format!(
+            "grant_type=refresh_token&refresh_token={}",
+            &self.refresh_token
+        );
+        let mut header = HeaderMap::new();
+        header.insert(
+            AUTHORIZATION,
+            HeaderValue::from_str(&*format!(
+                "Basic {}",
+                base64::encode(format!(
+                    "{}:{}",
+                    self.client_id.to_owned(),
+                    self.client_secret.to_owned()
+                ))
+            ))
+            .unwrap(),
+        );
+        header.insert(USER_AGENT, HeaderValue::from_str(user_agent).unwrap());
+        header.insert(
+            CONTENT_TYPE,
+            HeaderValue::from_str("application/x-www-form-urlencoded").unwrap(),
+        );
+        let response = client
+            .post(url)
+            .body(Body::from(body))
+            .headers(header)
+            .send()
+            .await
+            .map_err(InternalError::from)?;
+        response.status().into_result()?;
+
+        let token = response.text().await?;
+        let token: TokenResponseData = serde_json::from_str(token.as_str())?;
+        self.token = Some(token.access_token);
+        let x = token.expires_in * 1000;
+        let x1 = (x as u128)
+            + SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_millis();
+        self.expiration_time = Some(x1);
+        return Ok(true);
+    }
+    /// Logs out
+    async fn logout(&mut self, client: &Client, user_agent: &str) -> Result<(), Error> {
+        let url = "https://www.reddit.com/api/v1/revoke_token";
+        let body: String;
+        body = format!("token={}&token_type_hint=refresh_token", &self.refresh_token.to_owned());
+
+        let mut header = HeaderMap::new();
+        header.insert(USER_AGENT, HeaderValue::from_str(user_agent).unwrap());
+        header.insert(
+            CONTENT_TYPE,
+            HeaderValue::from_str("application/x-www-form-urlencoded").unwrap(),
+        );
+        let response = client
+            .post(url)
+            .body(Body::from(body))
+            .headers(header)
+            .send()
+            .await?;
+        response.status().into_result()?;
+        self.token = None;
+        self.expiration_time = None;
+        self.refresh_token = "".to_string();
+        Ok(())
+    }
+    /// Returns true if successful
+    async fn token_refresh(&mut self, client: &Client, user_agent: &str) -> Result<bool, Error> {
+        self.login(client, user_agent).await
+    }
+    /// headers
+    fn headers(&self, headers: &mut HeaderMap) {
+        headers.insert(
+            AUTHORIZATION,
+            HeaderValue::from_str(&*format!("Bearer {}", self.token.to_owned().unwrap())).unwrap(),
+        );
+    }
+    /// True
+    fn oauth(&self) -> bool {
+        true
+    }
+    /// Validates Time
+    fn needs_token_refresh(&self) -> bool {
+        let i = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+        if !self.refresh_token.is_empty() {
+            if self.expiration_time.is_none() {
+                true
+            } else {
+                i >= self.expiration_time.unwrap()
+            }
+        } else {
+            false
+        }
+    }
+    fn get_refresh_token(&self) -> Option<String> {
+        Some(self.refresh_token.to_owned())
+    }
+}
+
+#[async_trait]
+impl Authorized for TokenAuthenticator {}
